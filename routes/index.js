9a3282c2b7c728f826a75f32220ab076daaa084d
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-12-22 12:20:24 +0100

    'ITS ALIVE'-test done, application works using Nodemon

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Login page' });
+  console.log('Its ALIVE - Page is loaded');
 });
+console.log('Its ALIVE - Application is alive');
 
 module.exports = router;
