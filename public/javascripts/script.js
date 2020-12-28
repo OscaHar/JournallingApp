755889d719c1061fe0cc8e87a684d268710312af
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-12-28 13:16:27 +0100

    Made the add button functional, toggles the display on the elements

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+console.log('SCRIPT.JS LOADED');
+
+window.onload = function () {
+  document.querySelector('.circle').addEventListener('click', function () {
+    document.querySelector('.circle').classList.toggle('displayElement');
+    document
+      .querySelector('.header--BigText')
+      .classList.toggle('displayElement');
+    console.log('CLICK!');
+  });
+};
