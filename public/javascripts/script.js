e4a0661f86dd5f8ee8beddd5a11ecba70932fd1d
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-12-29 15:17:44 +0100

    Added a button that takes the page back to its original view. Made it so it saves the note on airtable when going back to the main page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,6 +8,25 @@ window.onload = function () {
     document
       .querySelector('.header--BigText')
       .classList.toggle('displayElement');
+    document.querySelector('.header--arrow').classList.toggle('displayElement');
+    document.querySelector('.noteInput2').classList.toggle('displayElement');
     console.log('CLICK!');
   });
+
+  document
+    .querySelector('.header--arrow')
+    .addEventListener('click', function () {
+      addData();
+      document.querySelector('.circle').classList.toggle('displayElement');
+      document
+        .querySelector('.header--BigText')
+        .classList.toggle('displayElement');
+      document
+        .querySelector('.header--arrow')
+        .classList.toggle('displayElement');
+
+      document.querySelector('.noteInput2').classList.toggle('displayElement');
+      document.querySelector('.noteInput2').textContent = '';
+      //TODO: Clear textarea
+    });
 };
