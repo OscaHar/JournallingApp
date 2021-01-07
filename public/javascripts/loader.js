1c2c36ed4a00bf3afa1b6eb9ab58f1896ff7121a
Oscar Harrysson <harrysson.oscar@gmail.com>
2021-01-07 19:12:02 +0100

    Made the wrapper div (parent) an id and using that to remove the bug that occured when adding a new note

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -3,6 +3,7 @@
 console.log('LOADER.JS LOADED');
 
 function loader() {
+  console.log('LOADER CALLED');
   fetch(
     `https://api.airtable.com/v0/app92BlzQFv5IKqof/journalData?api_key=keyoVMiz3n49GzOHb`
   )
@@ -12,21 +13,28 @@ function loader() {
 
       const { records } = data;
       const div = document.createElement('div');
+      div.id = 'parent2';
       div.classList.add('parent');
 
       var markup = '';
-
+      // data.sort(
+      //   (a, b) =>
+      //     parseFloat(a.records.fields.ID) - parseFloat(b.records.fields.ID)
+      // );
       for (var i = 0; i < data.records.length; i++) {
         markup += `
         <div class="note">
             <p>${data.records[i].fields.Notes}</p>
             <p>${data.records[i].fields.Date}</p>
-        </div>
+            </div>
         `;
 
         div.innerHTML = markup;
         document.getElementById('container').appendChild(div);
       }
     });
+  var removeParent = document.getElementById('parent2');
+  removeParent.parentNode.removeChild(removeParent);
 }
+
 loader();
