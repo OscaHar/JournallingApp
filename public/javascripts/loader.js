976ef30414b404e17b1321df8236928a08234882
Oscar Harrysson <harrysson.oscar@gmail.com>
2021-01-08 14:00:07 +0100

    Removed unnecessary console logs and code that is not being used more, added a sorting system that sorts the notes in the wished order.

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -3,38 +3,31 @@
 console.log('LOADER.JS LOADED');
 
 function loader() {
-  console.log('LOADER CALLED');
   fetch(
     `https://api.airtable.com/v0/app92BlzQFv5IKqof/journalData?api_key=keyoVMiz3n49GzOHb`
   )
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-
       const { records } = data;
       const div = document.createElement('div');
       div.id = 'parent2';
       div.classList.add('parent');
 
       var markup = '';
-      // data.sort(
-      //   (a, b) =>
-      //     parseFloat(a.records.fields.ID) - parseFloat(b.records.fields.ID)
-      // );
+      records.sort(
+        (a, b) => (b.fields.Sorting > a.fields.Sorting ? 1 : -1)
+        //parseFloat(a.fields.Sorting) - parseFloat(b.fields.Sorting)
+      );
       for (var i = 0; i < data.records.length; i++) {
         markup += `
         <div class="note">
-            <p>${data.records[i].fields.Notes}</p>
-            <p>${data.records[i].fields.Date}</p>
-            </div>
+        <p>${data.records[i].fields.Notes}</p>
+        <p>${data.records[i].fields.Date}</p>
+        </div>
         `;
 
         div.innerHTML = markup;
         document.getElementById('container').appendChild(div);
       }
     });
-  var removeParent = document.getElementById('parent2');
-  removeParent.parentNode.removeChild(removeParent);
 }
-
-loader();
